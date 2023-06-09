a2ee948ab8b24b7f2e92183e0e1e43960ba1f863
Augusto Manzano <81631170+J-AugustoManzano@users.noreply.github.com>
2023-06-08 22:10:49 -0300

    Add files via upload

---
+++ Codigos/Cap_08/Gab08_Ex02/Gab08_Ex02/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Gab08_Ex02
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            float F, C;
+            F = float.Parse(textBox1.Text);
+            C = ((F - 32) * 5) / 9;
+            label1.Text = C.ToString();
+        }
+    }
+}
---
+++ Codigos/Cap_08/Gab08_Ex03/Gab08_Ex03/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Gab08_Ex03
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int A, B, X;
+            A = int.Parse(textBox1.Text);
+            B = int.Parse(textBox2.Text);
+            X = A;
+            A = B;
+            B = X;
+            label1.Text = A.ToString();
+            label2.Text = B.ToString();
+        }
+    }
+}
