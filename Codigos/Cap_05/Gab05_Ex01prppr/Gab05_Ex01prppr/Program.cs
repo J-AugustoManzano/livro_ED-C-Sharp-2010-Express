c52d7beb93061f50c68a47210bcdf123253d8513
Augusto Manzano <81631170+J-AugustoManzano@users.noreply.github.com>
2023-06-08 22:07:34 -0300

    Add files via upload

---
+++ Codigos/Cap_05/Gab05_Ex01prppr/Gab05_Ex01prppr/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Gab05_Ex01prppr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int X;
+            long R;
+            Console.Write("Entre um valor numerico: ");
+            X = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            R = Somatorio(X);
+            Console.WriteLine("Somatorio de ate {0} = {1}", X, R);
+            Console.WriteLine();
+            Console.Write("Tecle algo para encerrar... ");
+            Console.ReadKey();
+
+        }
+
+        public static long Somatorio(int N)
+        {
+            long SOMA = 0;
+            for (int I = 1; I <= N; I++)
+                SOMA += I;
+            return SOMA;
+        }
+
+    }
+}
---
+++ Codigos/Cap_05/Gab05_Ex01prppv/Gab05_Ex01prppv/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Gab05_Ex01prppv
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int X;
+            Console.Write("Entre um valor numerico: ");
+            X = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            Somatorio(X);
+            Console.WriteLine();
+            Console.Write("Tecle algo para encerrar... ");
+            Console.ReadKey();
+
+        }
+
+        public static void Somatorio(int N)
+        {
+            long SOMA = 0;
+            for (int I = 1; I <= N; I++)
+                SOMA += I;
+            Console.WriteLine("Somatorio de ate {0} = {1}", N, SOMA);
+        }
+
+    }
+}
