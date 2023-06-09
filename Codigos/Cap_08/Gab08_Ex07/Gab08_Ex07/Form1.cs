6e54e3f2c81dabc20533a455ac9a92488bcb3529
Augusto Manzano <81631170+J-AugustoManzano@users.noreply.github.com>
2023-06-08 22:12:53 -0300

    Add files via upload

---
+++ Codigos/Cap_08/Gab08_Ex07/Gab08_Ex07/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Gab08_Ex07
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int SOMA, I;
+
+            SOMA = 0;
+            for (I = 1; I <= 100; I++)
+                SOMA += I;
+            MessageBox.Show(SOMA.ToString());
+        }
+    }
+}
